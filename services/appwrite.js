fd1d20fc1e3e7ddbf94b40c35e8dec958632f6e0
cy2 <support@flovent.online>
2026-05-18 19:06:25 +0100

    updated

--- services/appwrite.js
+++ services/appwrite.js
@@ -4,10 +4,21 @@
 //   str_id: '679aafba003acc64c279'
 // }
 
-const appWriteConfig = {
-  endpoint: 'https://cloud.appwrite.io/v1',
-  project_id: '69be57c50030c703bf4c',
-  str_id: '69be57ef0026f19ac76d'
-}
+// const appWriteConfig = {
+//   endpoint: 'https://cloud.appwrite.io/v1',
+//   project_id: '69be57c50030c703bf4c',
+//   str_id: '69be57ef0026f19ac76d'
+// }
 
-export default appWriteConfig
+// export default appWriteConfig
+
+/* eslint-disable */
+
+// changed it to appwrite em
+const appWriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  project_id: "6a03a2500039ddd0f5ee",
+  str_id: "6a03a280003e3784b9c1",
+};
+
+export default appWriteConfig;
